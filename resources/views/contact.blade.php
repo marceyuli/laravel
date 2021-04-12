0cc5f572914edd66794e7b8cb7e34a45d41eb949
Yuliana <yulianamarcela200@gmail.com>
2021-04-11 22:17:03 -0400

    Enviar Formularios

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,4 +3,12 @@
 @section('titulo','contact')
 @section('content')
 <h1>Contact</h1>
+    <form method="POST" action="{{route('contact')}}">
+        @csrf
+        <input name="name" placeholder="Nombre...."><br>
+        <input type="email" name="email" placeholder="Email..."><br>
+        <input name="subject" placeholder="Asunto...."><br>
+        <textarea name="content" placeholder="Mensaje..."></textarea><br>
+        <button>Enviar</button>
+    </form>
 @endsection
